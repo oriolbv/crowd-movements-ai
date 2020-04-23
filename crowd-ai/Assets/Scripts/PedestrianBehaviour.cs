3919101e81970dcb0f29e8c6d2cf6c25c4dc03fc
Oriol Burgaya <oriolbv@gmail.com>
2020-04-23 21:36:18 +0200

    Pedestrian behaviour script added

---
+++ crowd-ai/Assets/Scripts/PedestrianBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedestrianBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Material newMat = Resources.Load("RedMaterial", typeof(Material)) as Material;
+        this.GetComponent<Renderer>().material = newMat;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
