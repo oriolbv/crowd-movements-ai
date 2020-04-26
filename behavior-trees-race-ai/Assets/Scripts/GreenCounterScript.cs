99264d66749e324f0d336e6871aa8d1be2276f10
Oriol Burgaya <oriolbv@gmail.com>
2020-04-26 14:07:51 +0200

    Green Team behavior tree added

---
+++ behavior-trees-race-ai/Assets/Scripts/GreenCounterScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GreenCounterScript : MonoBehaviour
+{
+    private int _counter = 0;
+
+    public void incrementa()
+    {
+        ++_counter;
+        this.GetComponent<Text>().text = _counter.ToString();
+    }
+}
