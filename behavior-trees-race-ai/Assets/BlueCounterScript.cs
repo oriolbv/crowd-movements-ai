14f386404325877f0e5edb5a6b81acd44a8af884
Oriol Burgaya <oriolbv@gmail.com>
2020-04-26 18:12:19 +0200

    Tanks project added

---
+++ behavior-trees-race-ai/Assets/BlueCounterScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlueCounterScript : MonoBehaviour
+{
+    private int _counter = 0;
+
+    public void incrementBlue()
+    {
+        ++_counter;
+        this.GetComponent<Text>().text = _counter.ToString();
+    }
+}
